40278be95880e69fbf899b2c7337bc4f82c7f34d
JulianAtencio <juat699@gmail.com>
2013-07-23 12:03:47 -0500

    se modific clase

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out
                 .println("=============Aja Cambio al COMMIr de Julian Repo Programa de MODIFICADO DOS prueba para GIT CON hub============");
         ImprimeGrupo imprimeGrupo = new ImprimeGrupo();
-        // imprimeHead.devuelve_mensajeHead(true);//Hola
+        // imprimeHead.devuelve_mensajeHead(true);//Holaa
         imprimeGrupo.devuelve_mensajeGrupo(false);
     }
 }
